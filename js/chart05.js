7f89da3458109732165a5f14ea5789773d78d2e2
PeiwenChuah <peiwenchuah4321@gmail.com>
2026-05-03 20:52:28 +0800

    Initial commit

---
+++ js/chart05.js
@@ -0,0 +1,5 @@
+var spec = "json/chart05.vg.json";
+ 
+vegaEmbed('#chart05', spec).then(function(result) {
+}).catch(console.error);
+ 
---
+++ js/chart06.js
@@ -0,0 +1,6 @@
+var spec = "json/chart06.vg.json";
+ 
+vegaEmbed('#chart06', spec).then(function(result) {
+}).catch(console.error);
+ 
+ 
