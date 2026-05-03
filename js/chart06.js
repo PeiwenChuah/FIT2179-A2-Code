2df61a1b387958c84168af0efcfbb8700c0628ea
PeiwenChuah <peiwenchuah4321@gmail.com>
2026-05-03 21:32:06 +0800

    Change Chart 6 to Chart 9

--- js/chart06.js
+++
@@ -1,6 +0,0 @@
-var spec = "json/chart06.vg.json";
- 
-vegaEmbed('#chart06', spec).then(function(result) {
-}).catch(console.error);
- 
- 
---
+++ js/chart09.js
@@ -0,0 +1,6 @@
+var spec = "json/chart09.vg.json";
+ 
+vegaEmbed('#chart09', spec).then(function(result) {
+}).catch(console.error);
+ 
+ 
